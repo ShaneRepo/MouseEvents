4f79f9e5d5a2ed3b1e3f55ea669b664cd18a9a4b
Shane <shane8641@live.com>
2016-03-11 21:34:47 -0500

    added a more vivid image on mouse out and mouse over

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-
+function mOverFAQ() {
+    document.getElementById("faq").src = "faq2.gif";
+}
+function mOutFAQ() {
+    document.getElementById("faq").src = "faq1.gif";
+}
+function mOverGB() {
+    document.getElementById("guest").src = "guestbook2.gif";
+}
+function mOutGB() {
+    document.getElementById("guest").src = "guestbook1.gif";
+}
+function mOverHOME() {
+    document.getElementById("home").src = "home2.gif";
+}
+function mOutHOME() {
+    document.getElementById("home").src = "home1.gif";
+}
+function mOverJOIN() {
+    document.getElementById("join").src = "join2.gif";
+}
+function mOutJOIN() {
+    document.getElementById("join").src = "join1.gif";
+}
